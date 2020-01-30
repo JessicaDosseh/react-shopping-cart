bebbfec617080413b20ece83ecfaf713977e9403
Jessica <Jessica@users-iMac.local>
2020-01-29 22:14:13 -0500

    CartContext

--- src/App.js
+++ src/App.js
@@ -22,27 +22,31 @@ function App() {
 
 	return (
 		<ProductContext.Provider value={{ products, addItem }}>
-			<div className="App">
-				<Navigation cart={cart} />
+			<CartContext.Provider value={cart}>
+				<div className="App">
+					{/* <Navigation cart={cart} /> */}
+					<Navigation />
 
-				{/* Routes */}
-				{/* <Route
-					exact
-					path="/"
-					render={() => (
-						<Products
-							products={products}
-							addItem={addItem}
-						/>
-					)}
-				/> */}
-				<Route exact path="/" component={Products} />
+					{/* Routes */}
+					{/* <Route
+						exact
+						path="/"
+						render={() => (
+							<Products
+								products={products}
+								addItem={addItem}
+							/>
+						)}
+					/> */}
+					<Route exact path="/" component={Products} />
 
-				<Route
-					path="/cart"
-					render={() => <ShoppingCart cart={cart} />}
-				/>
-			</div>
+					{/* <Route
+						path="/cart"
+						render={() => <ShoppingCart cart={cart} />}
+					/> */}
+					<Route path='/cart' component={ShoppingCart} />
+				</div>
+			</CartContext.Provider>
 		</ProductContext.Provider>
 		
 	);
